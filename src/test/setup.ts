67c6d4a4d149294ee1a97cec249b0817d383b7fd
Dirac <daronmitchel@gmail.com>
2026-01-23 12:42:35 -0500

    feat: archivo de setup para tests

---
+++ src/test/setup.ts
@@ -0,0 +1,20 @@
+/**
+ * SETUP DE TESTS
+ */
+import '@testing-library/jest-dom'
+import { afterAll, afterEach, beforeAll } from 'vitest'
+import { setupServer } from 'msw/node'
+import { http, HttpResponse } from 'msw'
+
+/**
+ * Handlers de peticiones HTTP
+ */
+export const handlers = [
+
+]
+
+export const server = setupServer(...handlers)
+
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
